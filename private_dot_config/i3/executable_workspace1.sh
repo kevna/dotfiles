ca9e5090d2e999aae9bcb22b66b6ba8f0400906b
kevna <lakevna@gmail.com>
2020-07-29 19:00:30 +0000

    switch to official teams app

--- private_dot_config/i3/executable_workspace1.sh
+++ private_dot_config/i3/executable_workspace1.sh
@@ -1,4 +1,5 @@
 i3-msg 'workspace 01; append_layout ~/.config/i3/workspace1.json'
-snap run teams-for-linux &
+#snap run teams-for-linux &
+teams &
 firefox &
 kitty --name work_scratchbook -o shell nvim scratchbook.txt release.txt &
