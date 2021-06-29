f8800e966c63e015667c832abea9665ef2340155
kevna <lakevna@gmail.com>
2021-06-29 01:03:25 +0100

    awesome minor fixes

--- private_dot_config/awesome/screen.lua
+++ private_dot_config/awesome/screen.lua
@@ -101,7 +101,7 @@ screen.connect_signal("property::geometry", function(s)
     set_wallpaper(s)
     s.layouts = get_layouts(s)
     for tag in screen.tags do
-        awful.layout.set(s.layouts[1], t)
+        awful.layout.set(s.layouts[1], tag)
     end
 end)
 --
