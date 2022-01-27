db166005651e378e6e8b6e71eae212fc4a9a4aac
kevna <lakevna@gmail.com>
2022-01-27 23:27:13 +0000

    feat(i3): remove unused keyboard.sh

--- private_dot_config/i3/executable_keyboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-xmodmap - <<END
-remove Lock = Caps_Lock
-remove Control = Control_L
-keysym Caps_Lock = Control_L
-add Lock = Caps_Lock
-add Control = Control_L
-END
-xcape -e 'Shift_L=Shift_L|9;Shift_R=Shift_R|0;Control_L=Escape'
